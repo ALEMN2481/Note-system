52d982d6e8348984a4ebb3bbfdbf1725553fe24c
Juan Sebastian Ruiz <juansebasrr0106@gmail.com>
2024-09-20 22:06:42 -0500

    Solucionando Bugs

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -8,8 +8,9 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'myproject.sqlite'),
     )
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -24,10 +25,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello Wordl!'
     
     from . import db
     db.init_app(app)
@@ -35,9 +32,6 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
-    app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
 
     from . import  students
     app.register_blueprint(students.bp)
--- myproject/blog.py
+++ myproject/blog.py
@@ -1,12 +1,12 @@
-from flask import (
+from myproject import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from myproject.auth import login_required
+from myproject.db import get_db
 
-bp = Blueprint('students', __name__)
+bp = Blueprint('blog', __name__)
 
 @bp.route('/')
 def index():
@@ -18,40 +18,38 @@ def index():
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('students/index.html', posts=posts)
+    return render_template('blog/index.html', posts=posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 
 def create():
     if request.method == 'POST':
-        name = request.form['name']
-        birth_date = request.form['birth_date']
-        phone = request.form['phone']
-        nacional_id = request.form['nacional_id']
+        title = request.form['title']
+        body = request.form['body']
         error = None
 
-        if not name:
-            error = 'Name is required.'
+        if not title:
+            error = 'Title is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO students (name, birth_date, phone, nacional_id)'
+                'INSERT INTO post (title, body, author_id)'
                 ' VALUES (?, ?, ?)',
-                (name, birth_date, phone, nacional_id, g.user['id'])
+                (title, body, g.user['id'])
             )
             db.commit()
-            return redirect(url_for('students.index'))
+            return redirect(url_for('blog.index'))
 
-    return render_template('students/create.html')
+    return render_template('blog/create.html')
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, name, birth_date, phone, nacional_id'
-        ' FROM students p JOIN user u ON p.author_id = u.id'
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
     ).fetchone()
@@ -70,34 +68,32 @@ def update(id):
     post = get_post(id)
 
     if request.method == 'POST':
-        name = request.form['name']
-        birth_date = request.form['birth_date']
-        phone = request.form['phone']
-        nacional_id = request.form['nacional_id']
+        title = request.form['title']
+        body = request.form['body']
         error = None
 
-        if not name:
-            error = 'name is required.'
+        if not title:
+            error = 'Title is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE students SET name = ?, birth_date = ?, phone = ?, nacional_id = ? '
+                'UPDATE post SET title = ?, body = ?'
                 ' WHERE id = ?',
-                (name, birth_date, phone, nacional_id, id)
+                (title, body, id)
             )
             db.commit()
-            return redirect(url_for('students.index'))
+            return redirect(url_for('blog.index'))
 
-    return render_template('students/update.html', post=post)
+    return render_template('blog/update.html', post=post)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FROM students WHERE id = ?', (id,))
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
-    return redirect(url_for('students.index'))
+    return redirect(url_for('blog.index'))
